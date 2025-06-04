0d0eaaa7f9a2e0c69d8bf200eb575f8c49c72b13
Владимир <@DESKTOP-UCQJLL8>
2025-06-04 16:31:57 +0300

    Fixed all

--- BlazorShop.Client/Program.cs
+++ BlazorShop.Client/Program.cs
@@ -10,40 +10,23 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // Настройка логирования
 builder.Logging.SetMinimumLevel(LogLevel.Information);
 
-// Настройка HttpClient
-builder.Services.AddScoped<HttpClientHandler>(sp => new HttpClientHandler
-{
-    // Для разработки: игнорируем ошибки SSL
-    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true,
-    // Разрешаем автоматическое перенаправление
-    AllowAutoRedirect = true,
-    // Увеличиваем таймаут
-    UseCookies = true
-});
-
+// Настройка HttpClient - используем правильный URL сервера
 builder.Services.AddScoped(sp => {
-    var handler = sp.GetRequiredService<HttpClientHandler>();
-    var client = new HttpClient(handler)
+    var client = new HttpClient
     {
-        BaseAddress = new Uri("https://localhost:7006/"), // URL сервера API
-        Timeout = TimeSpan.FromSeconds(30) // Увеличиваем таймаут
+        BaseAddress = new Uri("https://localhost:7006/") // URL из launchSettings.json сервера
     };
-    
-    // Добавляем заголовки для отладки
-    client.DefaultRequestHeaders.Add("X-Client-App", "BlazorShop.Client");
-    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-    
+
     Console.WriteLine($"HttpClient настроен с базовым URL: {client.BaseAddress}");
-    
+
     return client;
 });
 
 // Регистрация сервисов
 builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<ProductService>();
 
 // Добавляем обработку ошибок
-builder.Services.AddScoped<IExceptionHandler>(sp => 
+builder.Services.AddScoped<IExceptionHandler>(sp =>
     new DefaultExceptionHandler(sp.GetRequiredService<ILogger<DefaultExceptionHandler>>()));
 
 await builder.Build().RunAsync();
